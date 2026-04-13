fbd6afbdcec37c8b3fcfd742694b80a32d453641
heidi04aldidi <gycdi18@gmail.com>
2026-04-13 16:10:11 +0530

    RateLimiter Initialized

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+app.use(RateLimiter);
+
 app.get('/', (req, res) => {
+    console.log('Server Hit')
     res.send('Hello!');
 });
+
+function RateLimiter(req, res, next){
+    console.log('RateLimiter initialized');
+    next();
+}
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
